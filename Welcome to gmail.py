7b8c0390075cca21a4696f1dbb72d1da5ff98bea
zoek917 <34751570+zoek917@users.noreply.github.com>
2018-01-09 14:42:15 -0500

    Add files via upload

---
+++ Welcome to gmail.py
@@ -0,0 +1,18 @@
+print ("Welcome to Gmail")
+myUsername= input ("Enter your username and then press return")
+
+if myUsername == 'zoek917':
+    print ("Username successful")
+else:
+    print ("Username incorrect")
+           
+
+
+
+
+
+
+
+
+
+
